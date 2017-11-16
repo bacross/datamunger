da484b6edc6123d4bd98f2f2bd8769e97d2c2928
bacross <bacross@hotmail.com>
2017-11-16 10:58:05 -0500

    change to setup for proper requirements

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(name='datamunger',
-    version='0.3.1',
+    version='0.3.2',
     description = 'Set of tools for Nan and Outlier Imputation',
     url = 'https://github.com/bacross/datamunger',
     author = 'bacross',
     author_email = 'pydatamunger@gmail.com',
     license='MIT',
     packages=['datamunger'],
-	install_requires=['numpy','pandas','sklearn','os','joblib'],
+	install_requires=['numpy','pandas','sklearn','joblib'],
     zip_safe=False)
